0f7ab358ddfdb561973442e2a64c9476099f72c8
rjmcf <rjm232@cam.ac.uk>
2016-11-24 17:33:49 +0000

    Implement While Statements

---
+++ src/miniJAST/WhileStmnt.java
@@ -0,0 +1,6 @@
+package miniJAST;
+
+public class WhileStmnt implements Statement {
+    private BoolExpression cond;
+    private Statement stmnt;
+}
---
+++ src/miniJAST/WhileStmntNoShortIf.java
@@ -0,0 +1,6 @@
+package miniJAST;
+
+public class WhileStmntNoShortIf implements StatementNoShortIf {
+    private BoolExpression cond;
+    private StatementNoShortIf stmnt;
+}
