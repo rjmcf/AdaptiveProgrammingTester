b721900dd07d6e63104d59db73ad63e05bb0b84b
rjmcf <rjm232@cam.ac.uk>
2016-12-07 18:56:28 +0000

    Remove Type from Expression

--- src/miniJAST/expressions/Expression.java
+++ src/miniJAST/expressions/Expression.java
@@ -2,10 +2,7 @@ package miniJAST.expressions;
 
 import miniJAST.Context;
 import miniJAST.expressions.returnValues.ReturnValues;
-import miniJAST.types.Type;
 
 public abstract class Expression {
-    protected Type type;
     public abstract ReturnValues evaluate(Context c) throws Exception;
-    public Type getType() { return type; }
 }
