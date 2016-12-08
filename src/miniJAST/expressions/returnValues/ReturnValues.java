0e23dd65dedc2dd7293e94e65e06e87e97f682b3
rjmcf <rjm232@cam.ac.uk>
2016-12-08 13:49:33 +0000

    Add getter methods

--- src/miniJAST/expressions/returnValues/ReturnValues.java
+++ src/miniJAST/expressions/returnValues/ReturnValues.java
@@ -11,4 +11,6 @@ public abstract class ReturnValues {
     public ReturnValues(UnannType t, int s, boolean a, boolean aa) { type = new Type(t,s); isArray = a; isArrayAccess = aa;}
 
     public Type getType() {return type;}
+    public boolean getIsArrayAccess() { return isArrayAccess; }
+    public boolean getIsArray() { return isArray; }
 }
