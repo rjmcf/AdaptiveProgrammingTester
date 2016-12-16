4266031d908e1a5aec565926196598f6c500e155
rjmcf <rjm232@cam.ac.uk>
2016-12-16 15:39:31 +0000

    Add new Map to Context

--- src/miniJAST/Context.java
+++ src/miniJAST/Context.java
@@ -8,9 +8,11 @@ import java.util.HashMap;
 public class Context {
     public HashMap<String, Type> namesToTypes;
     public HashMap<String, Object> namesToValues;
+    public HashMap<String, Integer> namesToDepths;
 
     public Context() {
         namesToTypes = new HashMap<>();
         namesToValues = new HashMap<>();
+        namesToDepths = new HashMap<>();
     }
 }
