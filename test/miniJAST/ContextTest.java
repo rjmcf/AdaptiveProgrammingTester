450d5e6061418b92b02bb6315637e7242036e18f
rjmcf <rjm232@cam.ac.uk>
2016-12-16 15:04:41 +0000

    Improve ContextTest to fail worse

--- test/miniJAST/ContextTest.java
+++ test/miniJAST/ContextTest.java
@@ -6,10 +6,10 @@ import miniJAST.expressions.Literal;
 import miniJAST.expressions.assignment.UnaryPostIncExpr;
 import miniJAST.expressions.boolExpr.RelationExpr;
 import miniJAST.expressions.boolExpr.RelationOp;
+import miniJAST.statements.Block;
 import miniJAST.statements.ForLoops.ForInit;
 import miniJAST.statements.ForLoops.ForStmnt;
 import miniJAST.statements.LocalVarDec;
-import miniJAST.statements.SingleWordStmnt;
 import miniJAST.statements.VarDeclarator;
 import miniJAST.types.Type;
 import miniJAST.types.UnannType;
@@ -20,14 +20,15 @@ import static org.testng.Assert.*;
 
 public class ContextTest {
     Context c;
-    VarDeclarator i;
-    LocalVarDec iDec;
+    VarDeclarator i, j;
+    LocalVarDec iDec, jDec;
     Literal zero, two;
-    Id iID;
+    Id iID, jID;
     RelationExpr lT2;
     UnaryPostIncExpr plus;
     ForInit init;
     ForStmnt fS;
+    Block b1;
 
     @BeforeMethod
     public void setUp() throws Exception {
@@ -48,19 +49,30 @@ public class ContextTest {
         lT2.setUpRelationExpr(RelationOp.LT, iID, two);
         plus = new UnaryPostIncExpr();
         plus.setUpPostIncExpr(true, iID);
+        j = new VarDeclarator();
+        j.setUpVarDec("j", iID);
+        jDec = new LocalVarDec();
+        jDec.setUpLVD(UnannType.INT);
+        jDec.addVarDec(j);
+        jID = new Id();
+        jID.setUpId(new Type(UnannType.INT, 1), "j");
         init = new ForInit();
         init.addLocalVarDec(iDec);
         fS = new ForStmnt();
         fS.setUpForStmnt(init, lT2);
         fS.addUpdate(plus);
-        fS.setBody(SingleWordStmnt.EMPTY);
+        b1 = new Block();
+        b1.addBlockStmnt(jDec);
+        fS.setBody(b1);
     }
 
     @Test
     public void testScope() throws Exception {
         /* Code is
-        for (int i = 0; i < 2; i++) ;
-        Check that i is not defined
+        for (int i = 0; i < 2; i++) {
+            int j = i;
+        }
+        Check that i and j are not defined
          */
         fS.execute(c);
         try {
