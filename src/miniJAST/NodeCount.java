943dd07f998b6f3d81c8a054f58d527dff8f18e7
rjmcf <rjm232@cam.ac.uk>
2016-12-31 15:46:34 +0000

    Add NodeCount

---
+++ src/miniJAST/NodeCount.java
@@ -0,0 +1,9 @@
+package miniJAST;
+
+public class NodeCount {
+    public int filled;
+    public int empty;
+
+    public NodeCount() { filled = 0; empty = 0; }
+    public NodeCount(int f, int e) { filled = f; empty = e; }
+}
