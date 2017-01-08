fc746baa5aa9f0b8e5a4a61c578e40a5254bb726
rjmcf <rjm232@cam.ac.uk>
2017-01-08 17:44:18 +0000

    Remove uneccessary code from Main.

--- src/Main.java
+++ src/Main.java
@@ -2,10 +2,5 @@ import questions.FillArrayToNExercise;
 
 public class Main {
 
-    public static void main(String[] args) {
-        FillArrayToNExercise test = new FillArrayToNExercise(10);
-        test.setUp();
-        test.runSolution();
-        System.out.println(test.checkSolved());
-    }
+    public static void main(String[] args) {    }
 }
