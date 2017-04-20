9bc8da5312cc6b45599fadd441f75019e6f35337
rjmcf <rjm232@cam.ac.uk>
2017-04-20 14:28:37 +0100

    Finish redrafting section 3.2

--- src/Main.java
+++ src/Main.java
@@ -3,8 +3,6 @@
 public class Main {
 
     public static void main(String[] args) {
-        int a[] = new int[3], i = 0;
-        a[i++] = i;
-        System.out.println(a[0]);
+        new IndexOutOfBoundsException()
     }
 }
