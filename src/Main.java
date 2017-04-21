4452b24fa8491391a3594796250f088cd0d56432
rjmcf <rjm232@cam.ac.uk>
2017-04-21 14:12:51 +0100

    Finish updating section 3.3

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,20 @@
-
+import miniJAST.expressions.Id;
+import miniJAST.expressions.Literal;
+import miniJAST.expressions.assignment.AssignExpr;
+import miniJAST.expressions.assignment.AssignOp;
+import miniJAST.statements.ExpressionStmnt;
+import miniJAST.types.PrimType;
 
 public class Main {
 
     public static void main(String[] args) {
-        new IndexOutOfBoundsException()
+        AssignExpr aE = new AssignExpr();
+        Id i = new Id();
+        i.setUpId("i");
+        Literal four = new Literal();
+        four.setUpLiteral(PrimType.INT, "4");
+        aE.setUpAssignExpr(i, AssignOp.EQ, four);
+        ExpressionStmnt eS = new ExpressionStmnt(aE);
+
     }
 }
