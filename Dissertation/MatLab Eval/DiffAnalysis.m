1704b0da0b390ce2b7d8a907af13b67bd11c37c8
rjmcf <rjm232@cam.ac.uk>
2017-05-14 23:15:01 +0100

    Add more comments

--- Dissertation/MatLab Eval/DiffAnalysis.m
+++ Dissertation/MatLab Eval/DiffAnalysis.m
@@ -1,18 +1,19 @@
 y = csvread('/Users/Rjmcf/Documents/Cambridge/Year 3/Personal Project/AdaptiveProgrammingTester/src/entryPoints/evaluationDiff.txt');
 x = 1:length(y);
-plot(x,y,'x');
-h = title('Differing rates of increase in difficulty for different exercises', 'FontSize', 16);
+plot(x,y,'x-');
+%h = title('Differing rates of increase in difficulty for different questions', 'FontSize', 16);
+h = title('Difficulty increase with number of correct solutions', 'FontSize', 16);
 P = get(h,'Position');
 set(h,'Position',[P(1) P(2)+0.3 P(3)])
-hold on;
-x = 0:length(y) + 3;
-y = 0.091 * x + 0.0;
-l1 = plot(x,y,'-');
-y = 0.067 * x + 0.66;
-l2 = plot(x,y,'-');
-y = 0.048 * x + 1.465;
-l3 = plot(x,y,'-');
-xlabel('Number of submissions', 'FontSize', 14)
+% hold on;
+% x = 0:length(y) + 3;
+% y = 0.091 * x + 0.0;
+% l1 = plot(x,y,'-');
+% y = 0.067 * x + 0.66;
+% l2 = plot(x,y,'-');
+% y = 0.048 * x + 1.465;
+% l3 = plot(x,y,'-');
+xlabel('Number of correct solutions', 'FontSize', 14)
 ylabel('Measured difficulty', 'FontSize', 14)
 xlim([0 35])
 ylim([0 3.2])
--- Dissertation/MatLab Eval/PerfAnalysis.m
+++ Dissertation/MatLab Eval/PerfAnalysis.m
@@ -6,7 +6,15 @@ Data = M(2:end, 2:end);
 
 clear title xlabel ylabel
 [C,h] = contour(X,Y,Data);
+hold on
 clabel(C,h)
+plot(0,4, 'r+')
+text(0,4,'B  ','HorizontalAlignment','right', 'FontSize', 14, 'Color', 'r')
+plot(7,2, 'm+')
+text(7,2,'C  ','HorizontalAlignment','right', 'FontSize', 14, 'Color', 'm')
+plot(0,2, 'b+')
+text(0,2,'A  ','HorizontalAlignment','right', 'FontSize', 14, 'Color', 'b')
+hold off
 h = title('Performance as a function of N^o of attempts taken and N^o of extra nodes', 'FontSize', 14);
 P = get(h,'Position');
 set(h,'Position',[P(1) P(2)+0.1 P(3)])
